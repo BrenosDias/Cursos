d4c85c0e27a2b60b1e95a38432170b699cc79648
BrenosDias <brenosdias@hotmail.com>
2024-06-19 18:52:02 -0300

    Sistema bancario basico

---
+++ DIO-Python-BackEnd/banco.py
@@ -0,0 +1,48 @@
+menu = """
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "d":
+        deposito = float(input("Quanto você deseja depositar?"))
+
+        saldo += deposito
+
+        extrato += f" Deposito R${deposito} |"
+
+        print(f"Seu deposito foi executado, seu saldo atual é de R$ {saldo}.")
+    elif opcao == "s":
+        if numero_saques < LIMITE_SAQUES:
+            saque = float(input("Qual você deseja sacar?"))
+            if(saque > 500):
+                print("O valor desejado supera o seu limite de R$ 500.00.")
+            else:
+                saldo -= saque
+                print(f"Seu saque foi executado, seu saldo atual é de R$ {saldo}.")
+
+                extrato += f" Saque R${saque} |"
+            numero_saques += 1 
+        else:
+            print("Você já atingiu o seu limite de saques diários")
+    elif opcao == "e":
+        print(extrato)
+            
+    elif opcao == "q":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
